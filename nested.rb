7c94cad17e5c7a246269f130bb198e1485d84f72
Andrea Toston <tostonar@gmail.com>
2019-07-23 14:37:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,6 +82,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
+		 programmer_hash[:yukihiro_matsumoto] = {
+		   :known_for => "Ruby",
+		    :languages => ["LISP", "C"]
+		 }
 
 
 end
